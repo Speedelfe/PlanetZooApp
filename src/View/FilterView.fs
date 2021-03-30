2415a1eb39d73ba39bb60611457c90ad0d329bef
Speedelfe <trautmann.n@gmail.com>
2021-03-30 20:26:43 +0200

    Toggle Buttons für Kontinent Filter

--- src/View/FilterView.fs
+++ src/View/FilterView.fs
@@ -13,21 +13,85 @@ module FilterView =
     open Avalonia.FuncUI.Components
     open Elmish
 
+    let viewContinentsFilterOptions : IView =
+        DockPanel.create [
+            DockPanel.dock Dock.Top
+            DockPanel.children [
+                TextBlock.create [
+                    TextBlock.dock Dock.Top
+                    TextBlock.fontSize 20.
+                    TextBlock.text "Continents"
+                ]
+                StackPanel.create [
+                    StackPanel.orientation Orientation.Horizontal
+                    StackPanel.dock Dock.Top
+                    StackPanel.margin (20., 10.)
+                    StackPanel.children [
+                        ToggleButton.create [
+                            //ToggleButton.isChecked isActive
+                            //ToggleButton.onClick (fun _ -> viewMode |> ChangeViewMode |> dispatch)
+                            ToggleButton.content "Afrika"
+                            ToggleButton.margin 10.
+                            ToggleButton.padding (40., 14.)
+                        ]
+                        ToggleButton.create [
+                            ToggleButton.content "Asia"
+                            ToggleButton.margin 10.
+                            ToggleButton.padding (40., 14.)
+                        ]
+                        ToggleButton.create [
+                            ToggleButton.content "Central America"
+                            ToggleButton.margin 10.
+                            ToggleButton.padding (40., 14.)
+                        ]
+                        ToggleButton.create [
+                            ToggleButton.content "Europe"
+                            ToggleButton.margin 10.
+                            ToggleButton.padding (40., 14.)
+                        ]
+                    ]
+                ]
+                StackPanel.create [
+                    StackPanel.orientation Orientation.Horizontal
+                    StackPanel.dock Dock.Top
+                    StackPanel.margin (20., 10.)
+                    StackPanel.children [
+                        ToggleButton.create [
+                            ToggleButton.content "South America"
+                            ToggleButton.margin 10.
+                            ToggleButton.padding (40., 14.)
+                        ]
+                        ToggleButton.create [
+                            ToggleButton.content "North America"
+                            ToggleButton.margin 10.
+                            ToggleButton.padding (40., 14.)
+                        ]
+                        ToggleButton.create [
+                            ToggleButton.content "Oceania"
+                            ToggleButton.margin 10.
+                            ToggleButton.padding (40., 14.)
+                        ]
+                    ]
+                ]
+            ]
+        ]
+        :> IView
+
     let viewFilterView (state: State) dispatch : IView =
         DockPanel.create [
             DockPanel.children [
-                //TODO: Pills für Kontinente
-                Button.create [
-                    Button.content "Filter nach Continent"
-                    Button.dock Dock.Top
-                    Button.onClick (
-                        (fun _ ->
-                            match state.continentListFilter with
-                            | Some _ -> dispatch CLearFilterContinent
-                            | None -> FilterAnimalListByContinent [ Europe ] |> dispatch),
-                        OnChangeOf state.continentListFilter
-                    )
-                ]
+                viewContinentsFilterOptions
+                // Button.create [
+                //     Button.content "Filter nach Continent"
+                //     Button.dock Dock.Top
+                //     Button.onClick (
+                //         (fun _ ->
+                //             match state.continentListFilter with
+                //             | Some _ -> dispatch CLearFilterContinent
+                //             | None -> FilterAnimalListByContinent [ Europe ] |> dispatch),
+                //         OnChangeOf state.continentListFilter
+                //     )
+                // ]
                 StackPanel.create [
                     StackPanel.dock Dock.Bottom
                     StackPanel.orientation Orientation.Horizontal
